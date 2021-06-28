f27756f441dd89c637682f783eb9f2a5532dbde7
ashikahmedfahim <ashikahmed96@gmail.com>
2021-06-29 00:24:11 +0600

    update

--- src/App.js
+++ src/App.js
@@ -9,16 +9,22 @@ function App() {
     <div className="container">
       <p className="display-6 text-center m-5">DSI Web Assignment</p>
       <div className="text-center mt-5 mb-1 form-group">
-        <input type="text" name="" id="" className="form-control" value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}}/>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search here . . . "
+          className="form-control"
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        />
         <button className="btn btn-success mt-3 mx-2 px-5 py-2">Search</button>
       </div>
       <div className="text-center">
-        <button className="btn btn-primary m-2 px-3 py-2">
-          Sort by name
-        </button>
-        <button className="btn btn-primary m-2 px-3 py-2">
-          Sort by Price
-        </button>
+        <button className="btn btn-primary m-2 px-3 py-2">Sort by name</button>
+        <button className="btn btn-primary m-2 px-3 py-2">Sort by Price</button>
       </div>
 
       <div className="row">
