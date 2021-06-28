2c49e85d0deb3f5953ba1a90bd3fd1e920c5643d
ashikahmedfahim <ashikahmed96@gmail.com>
2021-06-28 23:18:42 +0600

    data added and initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const productData = [];
+  var options = {
+    method: "GET",
+    url: "https://amazon-products1.p.rapidapi.com/search",
+    params: { country: "US", query: "macbook+pro", page: "1" },
+    headers: {
+      "x-rapidapi-key": "f22158101dmsh55cd764a559dc33p13e873jsnfb4a3f29d95b",
+      "x-rapidapi-host": "amazon-products1.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      productData.push(response.data.results);
+      console.log(productData);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  return <div>{productData && productData.map((product) => console.log(product))}</div>;
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    link: "https://www.amazon.com/AmazonBasics-Buddies-Bedding-Pillow-Blanket/dp/B0843ZNKG3/ref=sr_1_1_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMDI2ODU2NDVKMFlBTktGMTVLQyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+    name: "Amazon Basics Kids Bear Buddies Bedding Nap Set with Bear Pillow and Fleece Throw Blanket",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71gsJi5M-cL._AC_SX679_.jpg",
+    price: 24.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Kids-Fleece-Tail-Blanket/dp/B0843ZFZNX/ref=sr_1_2_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMDY1ODU2MFpZUFpDRk1PN1ZQWCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+    name: "Amazon Basics Kids Fleece Tail Blanket - 26'' x 56'', Space Rocket",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71%2BFpDGOoDL._AC_SY879_.jpg",
+    price: 14.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Kids-Easy-Wash-Microfiber-Bedding/dp/B0835JBTKJ/ref=sr_1_3_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-3-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMTAzMzM5MjNHNkEyRVVOVVQ3MVcmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+    name: "Amazon Basics Kids Easy-Wash Microfiber Bed-in-a-Bag Bedding Set - Full/Queen, Magical Mermaids",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81PbKlelusL._AC_SX679_.jpg",
+    price: 48.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Ultra-Soft-Micromink-Sherpa-Blanket/dp/B0843ZQHQ7/ref=sr_1_4_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMDg4ODQ0NDE5UVk2SzNORFE0NDYmd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+    name: "Amazon Basics Kids Ultra-Soft Micromink Sherpa Blanket - Throw, Mountain Mint Green",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81aIWemDXEL._AC_SY879_.jpg",
+    price: 22.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Quick-Dry-Bath-Towels-Cotton/dp/B07B6GC2R5/ref=sr_1_11?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-11",
+    name: "Amazon Basics Quick-Dry, Luxurious, Soft, 100% Cotton Towels, White - Set of 2 Bath Towels",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91kJ9iHjUVL._AC_SY879_.jpg",
+    price: 20.97,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Laughing-Patterned-Blanket-Stuffed/dp/B083G7ZV77/ref=sr_1_17_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-17-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMDkxODI1NjNBM1dLM01ENzMyTSZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=",
+    name: "Amazon Basics Kids Laughing Lions Patterned Throw Blanket with Stuffed Animal Lion",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81MMaAPTebL._AC_SX679_.jpg",
+    price: 20.02,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Elastic-Pockets/dp/B086ZKPCZT/ref=sr_1_18_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-18-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMDg0ODg0MTNTTU5RWlowUTRJWDkmd2lkZ2V0TmFtZT1zcF9tdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+    name: "Amazon Basics Soft Microfiber Sheet Set with Elastic Pockets - Twin, Tide Pool Simple Stripe",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91VhoX9q3XL._AC_SX679_.jpg",
+    price: 15.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Wall-Mounted-Recessed-Electric-Fireplace/dp/B08291MFPB/ref=sr_1_19_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-19-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRFNJREJYRUlHODUmZW5jcnlwdGVkSWQ9QTAyMDk0MjhBQTdXTUxYSE1JOUEmZW5jcnlwdGVkQWRJZD1BMDYyNjE2NDNWT1VUNVUySkpJWU8md2lkZ2V0TmFtZT1zcF9tdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl",
+    name: "Amazon Basics Wall-Mounted Recessed Electric Fireplace - 42-Inch, Black",
+    img: "https://images-na.ssl-images-amazon.com/images/I/61Z4vA1MLEL._AC_SX466_.jpg",
+    price: 20.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Performance-Bath-Towels-Piece/dp/B07HGRJWYR/ref=sr_1_24?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-24",
+    name: "Amazon Basics 6 Piece Performance Bath Towels Set - Hydro Blue",
+    img: "https://images-na.ssl-images-amazon.com/images/I/A1OuQnu6aXL._AC_SX679_.jpg",
+    price: 28.99,
+  },
+  {
+    link : "https://www.amazon.com/AmazonBasics-Dual-Performance-Bath-Towel/dp/B083JMMG5N/ref=sr_1_30?dchild=1&keywords=amazonbasics&pd_rd_r=612de30a-0e5c-49c4-af79-8853ab8f6570&pd_rd_w=jpI1V&pd_rd_wg=MxDgf&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=2FJ6NMDJ2RRVH6S7EXAM&qid=1624897743&sr=8-30",
+    name: "Amazon Basics Dual Performance Bath Towel - 4-Pack, Warm Stone",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91psnFZ3pML._AC_SX679_.jpg",
+    price: 37.77,
+  }
+];
