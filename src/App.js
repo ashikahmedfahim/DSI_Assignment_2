851987ac64fd11300685a432fda89019a7ca01cf
ashikahmedfahim <ashikahmed96@gmail.com>
2021-06-29 13:31:20 +0600

    final

--- src/App.js
+++ src/App.js
@@ -5,14 +5,44 @@ function App() {
   const [primaryData, setprimaryData] = useState(data);
   const [searchValue, setSearchValue] = useState("");
 
+  const search = () => {
+    if (searchValue === "") {
+      setprimaryData(data);
+    } else {
+      const searchedArray = primaryData.filter((dt) => {
+        return dt.name.includes(searchValue);
+      });
+      setprimaryData(searchedArray);
+    }
+  };
+
+  const sortByPriceLow = () => {
+    setprimaryData(()=>{return sortData("L")});
+  };
+
+  const sortByPriceHigh = () => {
+    setprimaryData(()=>{return sortData("H")});
+  };
+
+  const sortData = (value) => {
+    const prices = primaryData.map((dt) => dt.price);
+    let sortedPrice = prices.sort();
+    if(value === "H"){
+      sortedPrice.reverse();
+    }
+    const sortedData = [];
+    sortedPrice.map((price) =>
+      sortedData.push(primaryData.filter((dt) => dt.price === price)[0])
+    );
+    return sortedData;
+  };
+
   return (
     <div className="container">
       <p className="display-6 text-center m-5">DSI Web Assignment</p>
       <div className="text-center mt-5 mb-1 form-group">
         <input
           type="text"
-          name=""
-          id=""
           placeholder="Search here . . . "
           className="form-control"
           value={searchValue}
@@ -20,23 +50,38 @@ function App() {
             setSearchValue(e.target.value);
           }}
         />
-        <button className="btn btn-success mt-3 mx-2 px-5 py-2">Search</button>
+        <button
+          className="btn btn-success mt-3 mx-2 px-5 py-2"
+          onClick={search}
+        >
+          Search
+        </button>
       </div>
       <div className="text-center">
-        <button className="btn btn-primary m-2 px-3 py-2">Sort by name</button>
-        <button className="btn btn-primary m-2 px-3 py-2">Sort by Price</button>
+        <button
+          className="btn btn-primary m-2 px-3 py-2"
+          onClick={sortByPriceLow}
+        >
+          Sort by Price (Low to High)
+        </button>
+        <button
+          className="btn btn-primary m-2 px-3 py-2"
+          onClick={sortByPriceHigh}
+        >
+          Sort by Price (High to Low)
+        </button>
       </div>
 
       <div className="row">
         {primaryData &&
           primaryData.map((productDetails, i) => (
-            <div className="card col-4 m-3" style={{ width: "18rem" }}>
+            <div key={i} className="card col-4 m-3" style={{ width: "18rem" }}>
               <a href={productDetails.link}>
                 <img
                   className="card-img-top pt-1"
                   style={{ objectFit: "fill", height: "15rem", width: "15rem" }}
                   src={productDetails.img}
-                  alt="Card image cap"
+                  alt="Card cap"
                 ></img>
               </a>
               <div className="card-body">
